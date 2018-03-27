71fd5fc4c085f2a17eb294e17c07acd8e89f5b67
sbt_kotakahashi <kotakahashi@tech.softbank.co.jp>
2018-03-28 03:51:42 +0900

    プロジェクト ファイルを追加します。

---
+++ AspNetVstsSample.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetVstsSample.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
