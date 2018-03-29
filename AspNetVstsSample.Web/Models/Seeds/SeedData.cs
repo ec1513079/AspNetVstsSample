a1e9b93fc24beb36727a4b06b24f424b66d74120
ec1513079 <ec1513079@gmail.com>
2018-03-29 22:41:35 +0900

    Add Seed Data

---
+++ AspNetVstsSample.Web/Models/Seeds/SeedData.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetVstsSample.Web.Models.Seeds
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new AspNetVstsSampleDbContext(
+                serviceProvider.GetRequiredService<DbContextOptions<AspNetVstsSampleDbContext>>()))
+            {
+                // Look for any movies.
+                if (context.Samples.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Samples.AddRange(
+                    new Sample
+                    {
+                        Title = "Test Title by Seeding",
+                        Detail = "Test Detail by Seeding"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- AspNetVstsSample.Web/Program.cs
+++ AspNetVstsSample.Web/Program.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNetVstsSample.Web.Models;
+using AspNetVstsSample.Web.Models.Seeds;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace AspNetVstsSample.Web
@@ -14,7 +18,28 @@ namespace AspNetVstsSample.Web
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<AspNetVstsSampleDbContext>();
+                    // requires using Microsoft.EntityFrameworkCore;
+                    context.Database.Migrate();
+                    // Requires using RazorPagesMovie.Models;
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
